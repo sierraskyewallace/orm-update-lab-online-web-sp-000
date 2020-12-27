401c21bf4713d121b6891ae20a85515c0296967c
Sierra Wallace <sskwallace@gmail.com>
2020-12-27 01:16:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,8 @@
 require_relative "../config/environment.rb"
 
 class Student
-    attr_accessor :name, :grade 
-    attr_reader :id 
-    def initialize(name, grade, id=nil)
+    attr_accessor :name, :grade, :id
+      def initialize(name, grade, id=nil)
       @name = name
       @grade = grade
       @id = id
