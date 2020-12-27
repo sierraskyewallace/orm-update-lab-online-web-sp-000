7b871061d4c39a8bd2279d580c4e36194960f6b2
Sierra Wallace <sskwallace@gmail.com>
2020-12-27 01:18:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,28 +1,32 @@
 require_relative "../config/environment.rb"
 
 class Student
-    attr_accessor :name, :grade, :id
+    attr_accessor :name, :grade
+    attr_reader :id 
+    
       def initialize(name, grade, id=nil)
+      @id = id
       @name = name
       @grade = grade
-      @id = id
     end
     
     def self.create_table
       sql = <<-SQL 
-      CREATE TABLE students (
+      CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY, 
       name TEXT, 
       grade INTEGER)
       SQL
       DB[:conn].execute(sql)
     end
+    
     def self.drop_table 
       sql = <<-SQL 
       DROP TABLE students 
       SQL
       DB[:conn].execute(sql)
     end
+    
     def save 
       if self.id 
         self.update 
@@ -44,7 +48,7 @@ class Student
       id = row[0] 
       name = row[1]
       grade = row[2] 
-      student = self.new(name, grade) 
+      student = self.new(name, grade, id) 
       student
     end
     def self.find_by_name(name)
