44dbecb121a7641e6f739e470fb3b7c974f5ae54
Sierra Wallace <sskwallace@gmail.com>
2020-12-27 01:06:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,5 +36,27 @@ class Student
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
       end
     end
-      
+    def self.create(name, grade)
+      student = Student.new(name, grade)
+      student.save 
+      student 
+    end
+    def self.new_from_db(row)
+      Student.new(row[0], row[1], row[3])
+    end
+    def self.find_by_name(name)
+      sql = <<-SQL 
+      SELECT * 
+      FROM students 
+      WHERE name = ? 
+      LIMIT 1 
+      SQL
+      DB[:conn].execute(sql, name).map do |row|
+        self.new_from_db(row)
+      end.first
+    end
+    def update 
+      sql = "UPDATE students SET anme = ?, grade = ? WHERE id = ?"
+      DB[:conn].execute(sql, self.name, self,grade, self.id)
+    end
 end
