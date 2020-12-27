afeb3fb8a9a8a23a75be9e08b8b0520c9a625ebd
Sierra Wallace <sskwallace@gmail.com>
2020-12-27 01:11:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,6 +57,6 @@ class Student
     end
     def update 
       sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-      DB[:conn].execute(sql, self.name, self,grade, self.id)
+      DB[:conn].execute(sql, self.name, self.grade, self.id)
     end
 end
