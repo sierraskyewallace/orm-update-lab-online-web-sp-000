9a46e53fb049eace11f363d15e1faad7d7d5e542
Sierra Wallace <sskwallace@gmail.com>
2020-12-27 01:00:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,5 +24,14 @@ class Student
       SQL
       DB[:conn].execute(sql)
     end
-    
+    def save 
+      if self.id 
+        self.update 
+      else 
+        sql = <<-SQL 
+        INSERT INTO students (name, grade)
+        VALUES (?, ?) 
+        SQL
+        DB[:conn].execute(sql, self.name, self.grade) 
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students*)[0][0]")
 end
